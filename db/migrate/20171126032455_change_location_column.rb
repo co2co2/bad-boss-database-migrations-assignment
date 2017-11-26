4a226913195f541e93515b90e96be975e0b72a6e
co2co2 <coco8287@hotmail.com>
2017-11-25 23:14:48 -0500

    restore previous locations table by reset.

--- db/migrate/20171126032455_change_location_column.rb
+++
@@ -1,9 +0,0 @@
-class ChangeLocationColumn < ActiveRecord::Migration[5.0]
-  def change
-    change_table :locations do |t|
-      t.remove :category, :placename, :address
-      t.string :city
-      t.string :weather
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171126032455) do
+ActiveRecord::Schema.define(version: 20171124232953) do
 
   create_table "locations", force: :cascade do |t|
-    t.string "city"
-    t.string "weather"
+    t.string "category"
+    t.string "placename"
+    t.string "address"
   end
 
   create_table "parts", force: :cascade do |t|
